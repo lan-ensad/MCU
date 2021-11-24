00f4543ee4b6e32c98c691c4ec3ff4aa61a9c365
le LAN <58851935+lan-ensad@users.noreply.github.com>
2021-11-24 20:57:27 +0100

    .

---
+++ 3-btn-trig/3-btn-trig.ino
@@ -0,0 +1,63 @@
+#define NB 3
+
+const int BTN [3] = {2, 3, 4};
+
+boolean state [NB];
+boolean prevState [NB];
+
+int s = 3;
+
+void setup() {
+  Serial.begin(9600);
+  for (int i = 0; i < NB; i++) {
+    pinMode(BTN[i], INPUT_PULLUP);
+    Serial.print(i);
+  }
+}
+
+void Trig(int b, boolean s, boolean ps, int r) {
+  s = digitalRead(b);
+  if (!s) {
+    if (ps != s) {
+      s = r;
+    }
+  }
+  ps = s;
+}
+
+void loop() {
+  /*
+  //---- Array OK ----
+  for (int i = 0; i < NB ; i++) {
+    state[i] = digitalRead(BTN[i]);
+    if (!state[i]) {
+      if (prevState[i] != state[i]) {
+        s = i;
+      }
+    }
+    prevState[i] = state[i];
+  }
+  */
+
+  switch (s) {
+    case 0 :
+      Serial.println("STOP 2 et 3");
+      delay(2000);
+      s = 3;
+      break;
+    case 1 :
+      Serial.println("STOP 1 et 3");
+      delay(2000);
+      s = 3;
+      break;
+    case 2 :
+      Serial.println("STOP 1 et 2");
+      delay(2000);
+      s = 3;
+      break;
+    default :
+      break;
+  }
+
+  delay(200);
+}
