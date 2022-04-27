9f0df082ea443cb36decb1579743a34543e0737a
le LAN <58851935+lan-ensad@users.noreply.github.com>
2022-04-27 17:00:33 +0200

    Update esp32_robot_mqtt_control.ino

--- esp32_robot_mqtt_control/esp32_robot_mqtt_control.ino
+++ esp32_robot_mqtt_control/esp32_robot_mqtt_control.ino
@@ -33,8 +33,40 @@ void Stope(int v, int d) {
 }
 
 void loop() {
-  Tourne(255, 20);
-  delay(3000);
-  Stope(255, 5);
+  /*
+    Tourne(255, 20);
+    delay(3000);
+    Stope(255, 5);
+    delay(1000);
+  */
+  MA1_Forward(200);//Motor MA1 forward; PWM speed control
   delay(1000);
+  MA2_Backward(200);//Motor MA1 backward; PWM speed control
+  delay(1000);
+}
+
+void MA1_Forward(int Speed1)  //fast decay; Speed = High duty-cycle
+{
+  analogWrite(BACKIA1, Speed1);
+  digitalWrite(BACKIA2, LOW);
+}
+
+void MA2_Backward(int Speed1)  //slow decay; Speed = Low duty-cycle
+{
+  int Speed2 = 255 - Speed1;
+  analogWrite(BACKIA1, Speed2);
+  digitalWrite(BACKIA2, HIGH);
+}
+
+void MB1_Forward(int Speed1)
+{
+  analogWrite(BACKIB1, Speed1);
+  digitalWrite(BACKIB2, LOW);
+}
+
+void MB2_Backward(int Speed1)
+{
+  int Speed2 = 255 - Speed1;
+  analogWrite(BACKIB1, Speed2);
+  digitalWrite(BACKIB2, HIGH);
 }
