1d377fcf43e50fa7de05e2423dd409db2f43d87b
le LAN <58851935+lan-ensad@users.noreply.github.com>
2022-05-27 17:14:46 +0200

    Update porry_legacy.ino

--- _projects/porry_legacy/porry_legacy.ino
+++ _projects/porry_legacy/porry_legacy.ino
@@ -1,3 +1,5 @@
+//https://wiki.dfrobot.com/FireBeetle_ESP32_IOT_Microcontroller(V3.0)__Supports_Wi-Fi_&_Bluetooth__SKU__DFR0478
+
 #include <Arduino.h>
 #include <analogWrite.h>
 #include <WiFi.h>
@@ -11,6 +13,10 @@ const char* mqttServer = "192.168.0.101";
 const int mqttPort = 1883;
 String msg;
 
+const int FAN01 = 26;
+const int FAN02 = 25;
+const int FAN03 = 27;
+const int FAN04 = 9;
 bool fan01, fan02 = false;
 
 WiFiClient espClient;
@@ -21,27 +27,37 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived in topic: ");
   Serial.println(top);
   Serial.print("Message: ");
+  //==============
+  //=== FAN 01 ===
   if (top == "fans/01") {
     fan01 = !fan01;
+    if(fan01){digitalWrite(FAN01, HIGH);}else{digitalWrite(FAN01, LOW);}
     msg = "";
     for (int i = 0; i < length; i++) {
       msg += (char)payload[i];
     }
-    Serial.print(msg);Serial.print("\t \t");Serial.print("Fan 01 : ");Serial.println(fan01);
-  } else if (top == "fans/02") {
-    fan02 = !fan02;
-    msg = "";
-    for (int i = 0; i < length; i++) {
-      msg += (char)payload[i];
-    }
-    Serial.print(msg);Serial.print("\t \t");Serial.print("Fan 02 : ");Serial.println(fan02);
+    Serial.print(msg); Serial.print("\t \t"); Serial.print("Fan 01 : "); Serial.println(fan01);
   }
+  //==============
+  //=== FAN 02 ===
+  else if (top == "fans/02") {
+    fan02 = !fan02;
+    if(fan01){digitalWrite(FAN02, HIGH);}else{digitalWrite(FAN02, LOW);}
+    msg = "";
+    for (int i = 0; i < length; i++) {
+      msg += (char)payload[i];
+    }
+    Serial.print(msg); Serial.print("\t \t"); Serial.print("Fan 02 : "); Serial.println(fan02);
+  }
+  //==============
   Serial.println();
   Serial.println("-----------------------");
 }
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(FAN01, OUTPUT);pinMode(FAN02, OUTPUT);pinMode(FAN03, OUTPUT);pinMode(FAN04, OUTPUT);pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(FAN01, LOW);digitalWrite(FAN02, LOW);digitalWrite(FAN03, LOW);digitalWrite(FAN04, LOW);
+  
   Serial.begin(115200);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -64,14 +80,15 @@ void setup() {
   }
   client.subscribe("fans/01");
   client.subscribe("fans/02");
-}
-void loop(){
-  client.loop();
-
-  //====== Activation des pin relais ======
-  if(fan02){
+  for (int i = 0; i < 3; i++) {
     digitalWrite(LED_BUILTIN, HIGH);
-  }else{
+    delay(1000);
     digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
   }
+}
+void loop() {
+  client.loop();
+  if(fan01){digitalWrite(FAN01, HIGH);}else{digitalWrite(FAN01, LOW);}
+  if(fan02){digitalWrite(FAN02, HIGH);}else{digitalWrite(FAN02, LOW);}
 }
